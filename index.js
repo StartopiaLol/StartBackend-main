53c51d091f9af2bb29151afb66fdae6774919500
DrianZetax <116927325+DrianZetax@users.noreply.github.com>
2025-03-27 19:11:13 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const rateLimiter = require('express-rate-limit');
 const compression = require('compression');
+const path = require('path');
 
 app.use(compression({
     level: 5,
@@ -15,7 +15,12 @@ app.use(compression({
     }
 }));
 app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'public')));
 app.set('trust proxy', 1);
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+
 app.use(function (req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header(
@@ -25,21 +30,33 @@ app.use(function (req, res, next) {
     console.log(`[${new Date().toLocaleString()}] ${req.method} ${req.url} - ${res.statusCode}`);
     next();
 });
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.json());
-//app.use(rateLimiter({ windowMs: 15 * 60 * 1000, max: 100, headers: true }));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'html', 'index.html'));
+});
+
 app.all('/player/register', function(req, res) {
     res.send("Coming soon...");
 });
+
 app.all('/player/login/dashboard', function (req, res) {
     const tData = {};
     try {
-        const uData = JSON.stringify(req.body).split('"')[1].split('\\n'); const uName = uData[0].split('|'); const uPass = uData[1].split('|');
-        for (let i = 0; i < uData.length - 1; i++) { const d = uData[i].split('|'); tData[d[0]] = d[1]; }
-        if (uName[1] && uPass[1]) { res.redirect('/player/growid/login/validate'); }
-    } catch (why) { console.log(`Warning: ${why}`); }
-
-    res.render(__dirname + '/public/html/dashboard.ejs', {data: tData});
+        const uData = JSON.stringify(req.body).split('"')[1].split('\\n'); 
+        const uName = uData[0].split('|'); 
+        const uPass = uData[1].split('|');
+        for (let i = 0; i < uData.length - 1; i++) { 
+            const d = uData[i].split('|'); 
+            tData[d[0]] = d[1]; 
+        }
+        if (uName[1] && uPass[1]) { 
+            res.redirect('/player/growid/login/validate'); 
+            return;
+        }
+    } catch (why) { 
+        console.log(`Warning: ${why}`); 
+    }
+    res.render('dashboard', { data: tData });
 });
 
 app.all('/player/growid/login/validate', (req, res) => {
@@ -51,6 +68,7 @@ app.all('/player/growid/login/validate', (req, res) => {
         `{"status":"success","message":"Account Validated.","token":"${token}","url":"","accountType":"growtopia"}`,
     );
 });
+
 app.all('/player/growid/checktoken', (req, res) => {
     const { refreshToken } = req.body;
     res.json({
@@ -60,9 +78,6 @@ app.all('/player/growid/checktoken', (req, res) => {
         url: '',
         accountType: 'growtopia',
     });
-});
-app.get('/', function (req, res) {
-   res.send('Hello Memek');
 });
 
 app.listen(5000, function () {
